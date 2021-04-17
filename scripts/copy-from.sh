51f7532038e6544418b31a7b0c41c16248f22859
Thomas Weber <weber_t@mailbox.org>
2021-04-17 13:46:27 -0500

    Fix scratch-paint message uploading

--- scripts/copy-from.sh
+++ scripts/copy-from.sh
@@ -11,7 +11,7 @@ npm run build
 cd $start
 rm -r in
 mkdir -p in
-cp -r ../scratch-gui/translations/messages/src/ in
+cp -r ../scratch-gui/translations/messages/ in
 cp ../scratch-vm/src/extensions/tw/index.js in/tw.js
 
 echo DONE
